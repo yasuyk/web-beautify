7eecb1fb0b490443a9e8809ba86b1456ddcd4ae1
Yasuyuki Oka <yasuyk@gmail.com>
2013-11-16 22:26:37 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,119 @@
+# Synopsis
+
+**Web-beautify** is a formatting package of HTML, CSS and JavaScript/JSON for Emacs.
+It uses the command-line/node.js javascript formatter from http://jsbeautifier.org/ to format whole html, css, js or json files, or region.
+
+![](https://github.com/yasuyk/web-beautify/raw/master/image/web-beautify.gif)
+
+# Features
+* HTML, CSS, and JavaScript/JSON formatting
+* all settings are customizable (whitespace, formatting style, etc..) by `.jsbeautifyrc` configuration file.
+
+# Requirements
+
+* `js-beautify` installed by typing: `npm -g install js-beautify`
+
+# Installation
+
+## Manual
+
+Just drop `web-beautify.el`. somewhere in your `load-path`.
+
+```lisp
+(add-to-list 'load-path "~/somewhere")
+```
+
+## MELPA
+
+If you're an Emacs 24 user or you have a recent version of package.el
+you can install `web-beautify.el` from the [MELPA](http://melpa.milkbox.net/) repository.
+
+# Usage
+
+## Basic setup
+
+Add the following to your emacs init file.
+
+    (require 'web-beautify) ;; Not necessary if using ELPA package
+    (eval-after-load 'js2-mode
+      '(define-key js2-mode-map (kbd "C-c b") 'web-beautify-js))
+    (eval-after-load 'json-mode
+      '(define-key json-mode-map (kbd "C-c b") 'web-beautify-js))
+    (eval-after-load 'sgml-mode
+      '(define-key html-mode-map (kbd "C-c b") 'web-beautify-html))
+    (eval-after-load 'css-mode
+      '(define-key css-mode-map (kbd "C-c b") 'web-beautify-css))
+
+If you want to automatically format before saving a file,
+add the following hook to your emacs configuration:
+
+    (eval-after-load 'js2-mode
+      '(add-hook 'js2-mode-hook
+                 (lambda ()
+                   (add-hook 'before-save-hook 'web-beautify-js-buffer t t))))
+
+    (eval-after-load 'json-mode
+      '(add-hook 'json-mode-hook
+                 (lambda ()
+                   (add-hook 'before-save-hook 'web-beautify-js-buffer t t))))
+
+    (eval-after-load 'sgml-mode
+      '(add-hook 'html-mode-hook
+                 (lambda ()
+                   (add-hook 'before-save-hook 'web-beautify-html-buffer t t))))
+
+    (eval-after-load 'css-mode
+      '(add-hook 'css-mode-hook
+                 (lambda ()
+                   (add-hook 'before-save-hook 'web-beautify-css-buffer t t))))
+
+
+## `js-beautify` settings
+
+The following `js-beautify` settings are available with `.jsbeautifyrc`. Check out the official [jsbeautifier documentation](https://github.com/einars/js-beautify#options) for more details on the options:
+
+* `indent_size`: Indentation size [4]
+* `indent_char`: Indentation character [" "]
+* `indent_level`: Initial indentation level [0]
+* `indent_with_tabs`: Indent with tabs [false]
+* `preserve_newlines`: Preserve line-breaks [true]
+* `max_preserve_newlines`: Number of line-breaks to be preserved in one chunk [10]
+* `space_in_paren`: Add padding spaces within paren, ie. f( a, b ) [false]
+* `jslint_happy`: Enable jslint-stricter mode [false]
+* `brace_style`: \[collapse|expand|end-expand] ["collapse"]
+* `break_chained_methods`: Break chained method calls across subsequent lines [false]
+* `keep_array_indentation`: Preserve array indentation [false]
+* `unescape_strings`: Decode printable characters encoded in xNN notation [false]
+* `wrap_line_length`: Wrap lines at next opportunity after N characters [0]
+* `e4x`: Pass E4X xml literals through untouched [false]
+* `space_before_conditional`: should the space before conditional statement be added [true]
+* `keep_function_indentation`: false
+* `eval_code`: false
+
+### .jsbeautifyrc files
+**Web-beautify** supports `.jsbeautifyrc` JSON files.
+
+A hierarchy of `.jsbeautifyrc` files is supported, where rc files at the deeper levels override the settings from rc files at higher levels. For example, given the file structure listed below, formatting `/home/you/myProject/app.js` would inherit settings from: default -> `/home/you/myProject/.jsbeautifyrc`, while formatting `/home/you/myProject/tests/test.js` would inherit settings from: default -> `/home/you/myProject/.jsbeautifyrc` -> `/home/you/myProject/tests/.jsbeautifyrc`
+
+- /home/you/myProject/.jsbeautifyrc
+- /home/you/myProject/app.js
+- /home/you/myProject/tests/.jsbeautifyrc
+- /home/you/myProject/tests/test.js
+
+## Commands
+
+#### <kbd>M-x</kbd> `web-beautify-js`
+
+Format region from if active, otherwise the current buffer. Formation is done according to the `js-beautify` command.
+
+#### <kbd>M-x</kbd> `web-beautify-css`
+
+Format region from if active, otherwise the current buffer. Formation is done according to the `css-beautify` command.
+
+#### <kbd>M-x</kbd> `web-beautify-html`
+
+Format region from if active, otherwise the current buffer. Formation is done according to the `html-beautify` command.
+
+# License
+
+See [LICENSE](https://github.com/yasuyk/web-beautify/blob/master/LICENSE) for details.
---
+++ web-beautify.el
@@ -0,0 +1,153 @@
+;;; web-beautify.el --- Format HTML, CSS and JavaScript/JSON
+
+;; Copyright (C) 2013 Yasuyuki Oka <yasuyk@gmail.com>
+
+;; Author: Yasuyuki Oka <yasuyk@gmail.com>
+;; Version: 0.1
+;; URL: https://github.com/yasuyk/web-beautify
+;; Package-Requires:
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;; Code:
+
+(defvar web-beautify-html-program "html-beautify"
+  "The executable to use for formatting HTML.")
+
+(defvar web-beautify-css-program "css-beautify"
+  "The executable to use for formatting CSS.")
+
+(defvar web-beautify-js-program "js-beautify"
+  "The executable to use for formatting JavaScript and JSON.")
+
+(defconst web-beautify-args '("-f" "-"))
+
+(defun web-beautify-command-not-found-message (program)
+  "Construct a message about PROGRAM not found."
+  (format
+   "%s not found. Install it by typing: \"npm -g install js-beautify\" "
+   program))
+
+(defun web-beautify-format-error-message (bufname)
+  "Construct a format error message with BUFNAME."
+  (format
+   "Could not apply web-beautify. See %s to check errors for details"
+   bufname))
+
+(defun web-beautify-format-region (program beg end)
+  "By PROGRAM, format each line in the BEG .. END region."
+  (if (executable-find program)
+      (save-excursion
+        (apply 'call-process-region beg end program t
+               (list t nil) t web-beautify-args))
+    (message (web-beautify-command-not-found-message program))))
+
+(defun web-beautify-format-buffer (program extenstion)
+  "By PROGRAM, format current buffer with EXTENSTION."
+    (if (executable-find program)
+        (web-beautify-format-buffer-1 program extenstion)
+      (message (web-beautify-command-not-found-message program))))
+
+(defun web-beautify-format-buffer-1 (program extenstion)
+  "Internal function of `web-beautify-format-buffer'.
+
+By PROGRAM, format current buffer with EXTENSTION."
+  (let* ((tmpfile (make-temp-file "web-beautify" nil
+                                  (format ".%s" extenstion)))
+         (outputbufname (format "*web-beautify-%s*" extenstion))
+         (outputbuf (get-buffer-create outputbufname))
+         (args (append web-beautify-args (list tmpfile))))
+    (unwind-protect
+        (progn
+          (with-current-buffer outputbuf (erase-buffer))
+          (write-region nil nil tmpfile)
+
+          (if (zerop (apply 'call-process program nil outputbuf nil args))
+              (let ((p (point)))
+                (save-excursion
+                  (with-current-buffer (current-buffer)
+                    (erase-buffer)
+                    (insert-buffer-substring outputbuf)))
+                (goto-char p)
+                (message "Applied web-beautify")
+                (kill-buffer outputbuf))
+            (message (web-beautify-format-error-message outputbufname))
+            (display-buffer outputbuf)))
+      (progn
+        (delete-file tmpfile)))))
+
+;;;###autoload
+(defun web-beautify-html ()
+  "Format region if active, otherwise the current buffer.
+
+Formation is done according to the html-beautify command."
+  (interactive)
+  (if (use-region-p)
+      (web-beautify-format-region
+       web-beautify-html-program
+       (region-beginning) (region-end))
+    (web-beautify-html-buffer)))
+
+;;;###autoload
+(defun web-beautify-html-buffer ()
+  "Format the current buffer according to the html-beautify command."
+  (web-beautify-format-buffer web-beautify-html-program "html"))
+
+;;;###autoload
+(defun web-beautify-css ()
+  "Format region if active, otherwise the current buffer.
+
+Formation is done according to the css-beautify command."
+  (interactive)
+  (if (use-region-p)
+      (web-beautify-format-region
+       web-beautify-css-program
+    (region-beginning) (region-end))
+    (web-beautify-css-buffer)))
+
+;;;###autoload
+(defun web-beautify-css-buffer ()
+  "Format the current buffer according to the css-beautify command."
+  (web-beautify-format-buffer web-beautify-css-program "css"))
+
+;;;###autoload
+(defun web-beautify-js ()
+  "Format region from if active, otherwise the current buffer.
+
+Formation is done according to the js-beautify command."
+  (interactive)
+  (if (use-region-p)
+      (web-beautify-format-region
+       web-beautify-js-program
+       (region-beginning) (region-end))
+    (web-beautify-js-buffer)))
+
+;;;###autoload
+(defun web-beautify-js-buffer ()
+  "Format the current buffer according to the js-beautify command.
+
+Formation is done according to the js-beautify command."
+  (web-beautify-format-buffer web-beautify-js-program "js"))
+
+
+(provide 'web-beautify)
+
+;; Local Variables:
+;; coding: utf-8
+;; eval: (checkdoc-minor-mode 1)
+;; End:
+
+;;; web-beautify.el ends here
